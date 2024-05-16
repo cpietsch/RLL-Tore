8158d664fcacc8fc1bdad3f0e97ea83b2a0d70ce
Christopher Pietsch <cpietsch@gmail.com>
2024-05-16 21:59:57 +0200

    update test

--- server/test-input.py
+++ server/test-input.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 
 import time
-
 import automationhat
 
 if automationhat.is_automation_hat():
     automationhat.light.power.write(1)
 
-while True:
-    print("ADC:  {:6.3f}".format(automationhat.analog.read()))
-    print("Input 1: {}".format(automationhat.input.one.read()))
-    print("Input 2: {}".format(automationhat.input.two.read()))
-    print("Input 3: {}".format(automationhat.input.three.read()))
+DEBOUNCE = 1
 
-    time.sleep(0.1)
+tor1_state = { "time": 0, "state": 0, "count": 0 }
+tor2_state = { "time": 0, "state": 0, "count": 0 }
+
+while True:
+    tor1 = automationhat.input.one.read()
+    tor2 = automationhat.input.two.read()
+
+    if tor1 and tor1_state["state"] == 0 and time.time() - tor1_state["time"] > DEBOUNCE:
+        tor1_state["count"] += 1
+        print("TOR1: {}".format(tor1_state["count"]))
+
+    if tor1 != tor1_state["state"]:
+        tor1_state["time"] = time.time()
+        tor1_state["state"] = tor1
+
+    if tor2 and tor2_state["state"] == 0 and time.time() - tor2_state["time"] > DEBOUNCE:
+        tor2_state["count"] += 1
+        print("TOR2: {}".format(tor2_state["count"]))
+
+    if tor2 != tor2_state["state"]:
+        tor2_state["time"] = time.time()
+        tor2_state["state"] = tor2
+    
+
+    time.sleep(0.02)
