cb0a3a095fcf89aca0a44e4325e4a9fc5138adf6
Christopher Pietsch <cpietsch@gmail.com>
2024-05-18 13:29:11 +0200

    chore: Update kiosk setup and configuration
    This commit updates the kiosk setup and configuration by increasing the sleep time to 9 seconds before launching Chromium in kiosk mode. It also sets the display settings and disables screen blanking and power saving features. These changes aim to improve the stability and reliability of the kiosk feature.

--- linux/kiosk/kiosk.sh
+++ linux/kiosk/kiosk.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-xset s noblank
-xset s off
-xset -dpms
-
-DISPLAY=:0 xrandr --output HDMI-2 --same-as HDMI-1 &
+sleep 10
+DISPLAY=:0 xrandr --output HDMI-2 --same-as HDMI-1
+DISPLAY=:0 xset s noblank
+DISPLAY=:0 xset s off
+DISPLAY=:0 xset -dpms
 
 unclutter -idle 0.5 -root &
 
@@ -11,7 +11,7 @@ sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chr
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
 
 
-sleep 10  && /usr/bin/chromium-browser --noerrdialogs --disable-infobars --app="http://localhost/screen.html" --user-data-dir=$(mktemp -d) --enable-features=OverlayScrollbar --disable-pinch --kiosk &
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --app="http://localhost/screen.html" --user-data-dir=$(mktemp -d) --enable-features=OverlayScrollbar --disable-pinch --kiosk &
 
 while true; do
 #   xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
