c952abf1605a36c885318dbd41c4609fb716c614
Christopher Pietsch <cpietsch@gmail.com>
2024-05-29 08:12:48 +0200

    sleep 2

--- linux/kiosk/kiosk.sh
+++ linux/kiosk/kiosk.sh
@@ -10,7 +10,8 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
 
-
+sleep 2
+DISPLAY=:0 xrandr --output HDMI-2 --same-as HDMI-1
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --app="http://localhost/screen.html" --user-data-dir=$(mktemp -d) --enable-features=OverlayScrollbar --disable-pinch --kiosk &
 
 while true; do
